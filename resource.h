094a22b9b57d627bbf594b9c29907f1126c01eb0
Petar Huljek <petar.huljek@gmail.com>
2024-09-04 21:43:50 +0200

    Added ListTab Ui.

--- resource.h
+++ resource.h
@@ -19,14 +19,23 @@
 #define IDC_COMBOBOXEX3                 1012
 #define IDC_BUTTON1                     1013
 #define IDC_BUTTON2                     1014
+#define IDC_BUTTON3                     1015
+#define IDC_EDIT1                       1016
+#define IDC_EDIT2                       1017
+#define IDC_EDIT3                       1018
+#define IDC_EDIT4                       1019
+#define IDC_EDIT5                       1020
+#define IDC_EDIT6                       1021
+#define IDC_BUTTON4                     1022
+#define IDC_LIST1                       1023
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_RESOURCE_VALUE        136
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_CONTROL_VALUE         1024
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
